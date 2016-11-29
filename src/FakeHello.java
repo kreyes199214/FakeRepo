fb44163b180c967f5560cd5d791821497fa03232
Kimberly <kreyes199214@gmail.com>
2016-11-28 18:54:12 -0800

    starting fake project

---
+++ src/FakeHello.java
@@ -0,0 +1,11 @@
+
+public class FakeHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("hello fake world ");
+				 int age =10;
+				 System.out.println("hello fake world " + age +  " years");
+	}
+
+}
